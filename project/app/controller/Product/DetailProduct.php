1176fbfa4cc761dbd570bb45539fa4b3a331245c
callmenosaint <otakuovn@gmail.com>
2024-11-17 14:05:27 +0700

    sua include_once de tranh loi

--- project/app/controller/Product/DetailProduct.php
+++ project/app/controller/Product/DetailProduct.php
@@ -4,9 +4,9 @@ class DetailProduct
     public function getProductById($id)
     {
         if ($id !== "") {
-            include "app/view/Header.php";
-            include "app/view/Product/DetailProduct.php";
-            include "app/view/Footer.php";
+            include_once "app/view/Header.php";
+            include_once "app/view/Product/DetailProduct.php";
+            include_once "app/view/Footer.php";
         } else {
             echo "Không có giá trị id truyền vào";
         }
